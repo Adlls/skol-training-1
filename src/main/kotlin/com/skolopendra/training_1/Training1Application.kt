190729dd61f082ee1434717ce45ec8e15a9ec2be
Adlls <adlansaidullaev@gmail.com>
2021-08-24 15:10:11 +0300

    first commit

---
+++ src/main/kotlin/com/skolopendra/training_1/Training1Application.kt
@@ -0,0 +1,11 @@
+package com.skolopendra.training_1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Training1Application
+
+fun main(args: Array<String>) {
+	runApplication<Training1Application>(*args)
+}
